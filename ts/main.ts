660f65b08a5c587f54e9f5a4faf61fc48e1339a2
grebett <gregory.betton@gmail.com>
2016-02-05 11:25:28 +0100

    exercice: 1.6 Classes

--- ts/main.ts
+++ ts/main.ts
@@ -1,24 +1,19 @@
-interface Point {
-	x: number;
-	y: number;
-}
-
-function getX(p: Point): number {
-	return p.x;
-}
-
-class Point3D {
-	x: number;
-	y: number;
-	z: number;
+class BankAccount {	
+	constructor(public balance: number) {
+	}
 	
-	constructor(x: number, y: number, z: number) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
+	deposit(credit: number) {
+		this.balance += credit;
+		return this.balance;
 	}
 }
 
-var point = new Point3D(1, 2, 3)
-
-console.log(getX(point))
+class MyBankAccount extends BankAccount {
+	constructor(balance: number) {
+		super(balance);
+	}
+	
+	writeCheck(amount: number) {
+		this.balance -= amount;
+	}
+}
