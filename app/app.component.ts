aade6bb17cd0b81c66549a4d7aeac4071da9d2a4
grebett <gregory.betton@gmail.com>
2016-02-06 17:30:19 +0100

    example form complete

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,13 @@
 import {Component} from 'angular2/core';
+import {FormComponent} from './form.component';
 
 @Component({
 	selector: 'my-app',
-	templateUrl: './app/templates/component.html',
-	styleUrls: ['./app/styles/app.component.css']
+	templateUrl: './app/templates/app.component.html',
+	styleUrls: ['./app/styles/app.component.css'],
+	directives: [FormComponent]
 })
 
 export class AppComponent {
 	title = 'Welcome to my super page!';
 }
-
