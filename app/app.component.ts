91ff74f1f349c3616017fe27db877623188f3276
grebett <gregory.betton@gmail.com>
2016-02-06 21:17:13 +0100

    reset

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,10 @@
 import {Component} from 'angular2/core';
-import {FormComponent} from './form.component';
 
 @Component({
 	selector: 'my-app',
 	templateUrl: './app/templates/app.component.html',
-	styleUrls: ['./app/styles/app.component.css'],
-	directives: [FormComponent]
+	styleUrls: ['./app/styles/app.component.css']
 })
 
 export class AppComponent {
-	title = 'Welcome to my super page!';
 }
