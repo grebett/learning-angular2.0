bdf4d2f344cd86b27b38821738d2b6fbad2fcfb4
grebett <gregory.betton@gmail.com>
2016-02-06 15:42:08 +0100

    my-app component

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: './app/templates/component.html',
+	styleUrls: ['./app/styles/app.component.css']
+})
+
+export class AppComponent {
+	title = 'Welcome to my super page!';
+}
+
