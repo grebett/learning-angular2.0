028e61f058bcd628230d9dafc1880d05be174e80
grebett <gregory.betton@gmail.com>
2016-02-06 21:32:47 +0100

    switchy

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,4 +7,17 @@ import {Component} from 'angular2/core';
 })
 
 export class AppComponent {
+	public value: number = 42
+	switchy() {
+		switch (this.value) {
+			case 42:
+				return "Ah, the question...";
+				break;
+			case 0:
+				return "...noitseuq eht, hA";
+				break;
+			default:
+				return "Please enter 42 or 24!";
+		}
+	}
 }
