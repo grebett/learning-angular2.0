446cf9cc87446bc40329450f15b1178bdf94dbab
Gregory BETTON <grebett@e2r6p1.42.fr>
2016-02-08 16:47:34 +0100

    simple structural directive

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,13 @@
 import {Component} from 'angular2/core';
+import {UnlessDirective} from './unless.directive';
 
 @Component({
 	selector: 'my-app',
 	templateUrl: './app/templates/app.component.html',
-	styleUrls: ['./app/styles/app.component.css']
+	styleUrls: ['./app/styles/app.component.css'],
+	directives: [UnlessDirective]
 })
 
 export class AppComponent {
+	public n: number = 0;
 }
