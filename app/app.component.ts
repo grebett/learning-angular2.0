c732dda91aaf7abfed6c2b739351d1da6b7bb773
Gregory BETTON <grebett@e2r1p1.42.fr>
2016-02-09 13:59:37 +0100

    get data

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,30 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {ResourceService} from './resource.service';
 
 @Component({
 	selector: 'my-app',
 	templateUrl: './app/templates/app.component.html',
-	styleUrls: ['./app/styles/app.component.css']
+	styleUrls: ['./app/styles/app.component.css'],
+	providers: [ResourceService, HTTP_PROVIDERS]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+	resources: any[];
+	errorMessage: string;
+	
+	constructor(private _resourceService: ResourceService) {
+	}
+	
+	ngOnInit() {
+		this.getResource();
+	}
+
+	getResource() {
+		this._resourceService.getResource()
+			.subscribe(
+				resource => this.resources = resource,
+				error => this.errorMessage = error
+			);
+	}
 }
--- app/resource.service.ts
+++ app/resource.service.ts
@@ -1,1 +1,25 @@
+import {Injectable} from 'angular2/core';
+import {Http, Response} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
 
+@Injectable()
+
+export class ResourceService {
+	private url: string;
+
+	constructor(private http: Http) {
+		this.url = 'http://www.json-generator.com/api/json/get/cgsfeKmQgO?indent=2';
+	}
+
+	getResource() {
+		return this.http.get(this.url)
+						.map(res => res.json().data)
+						.do(data => console.log(data))
+						.catch(this.handleError);
+	}
+
+	handleError(error: Response) {
+		console.error(error);
+		return Observable.throw(error.json().error || 'Server error');
+	}
+}
